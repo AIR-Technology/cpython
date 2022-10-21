286e93877c8761d7afa4b1ba093fea694a109db8
prachkh <prach@airtechnology.io>
2022-10-21 13:38:45 +0700

    add command to copy python runtime to transparency installation directory

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,2 +1,6 @@
 ./configure --enable-shared --enable-optimizations
 make clean; make
+mkdir -p /opt/Coreograph/python/lib/python3.12/lib-dynload
+cp python /opt/Coreograph/python
+cp -r Lib/. /opt/Coreograph/python/lib/python3.12
+cp -r build/lib.linux-x86_64-3.12/. /opt/Coreograph/python/lib/python3.12/lib-dynload

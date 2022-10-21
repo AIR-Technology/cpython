36f005172c931c91d4d9f249470b457b7e3937ac
prachkh <prach@airtechnology.io>
2022-10-22 00:59:26 +0700

    fix lib-dynload source directory

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,6 +1,7 @@
+rm -rf /opt/Coreograph/python
 ./configure --enable-shared --enable-optimizations
 make clean; make
 mkdir -p /opt/Coreograph/python/lib/python3.12/lib-dynload
 cp python /opt/Coreograph/python
 cp -r Lib/. /opt/Coreograph/python/lib/python3.12
-cp -r build/lib.linux-x86_64-3.12/. /opt/Coreograph/python/lib/python3.12/lib-dynload
+cp -r Modules/*.so /opt/Coreograph/python/lib/python3.12/lib-dynload

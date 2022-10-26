d7707c3120cd845f7ce7e2fe8d1eef93f1bcd105
prachkh <prach@airtechnology.io>
2022-10-26 17:06:29 +0700

    change back to use system default libcrypto

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,7 +1,7 @@
 rm -rf /opt/Coreograph/python
 mkdir -p /opt/Coreograph/python
 sudo chown -R $USER:$USER /opt/Coreograph/python
-./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --enable-shared --enable-optimizations --with-openssl=../aws-lc/aws-lc-install
+./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --enable-shared --enable-optimizations
 make clean; make altinstall
 
 # upgrade pip

a438da906fff4eda259eb0678847ee21a97317ba
prachkh <prach@airtechnology.io>
2022-10-26 11:14:09 +0700

    use aws-lc as libcrypto

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,5 +1,11 @@
 rm -rf /opt/Coreograph/python
 mkdir -p /opt/Coreograph/python
 sudo chown -R $USER:$USER /opt/Coreograph/python
-./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --enable-shared --enable-optimizations
+./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --enable-shared --enable-optimizations --with-openssl=../aws-lc/aws-lc-install
 make clean; make altinstall
+
+# upgrade pip
+/opt/Coreograph/python/bin/python3.12 -m pip install --upgrade pip
+
+# thrid-party libs
+/opt/Coreograph/python/bin/python3.12 -m pip install opencv-python

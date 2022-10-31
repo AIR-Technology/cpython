90c581252a1c29c2bf1ae3ea44803a9f6b29afd3
prachkh <prach@airtechnology.io>
2022-10-31 16:44:40 +0700

    - fix openssl installation path

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,11 +1,11 @@
 rm -rf /opt/Coreograph/python
 mkdir -p /opt/Coreograph/python
 sudo chown -R $USER:$USER /opt/Coreograph/python
-./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --enable-shared --enable-optimizations
+./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --with-openssl=../openssl/openssl-install --enable-shared --enable-optimizations
 make clean; make altinstall
 
 # upgrade pip
 /opt/Coreograph/python/bin/python3.12 -m pip install --upgrade pip
 
 # thrid-party libs
-/opt/Coreograph/python/bin/python3.12 -m pip install opencv-python
+/opt/Coreograph/python/bin/python3.12 -m pip install opencv-python-headless

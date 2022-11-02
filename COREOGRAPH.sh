409e1e644c022986b345e11d442ecd6c694e267a
prachkh <prach@airtechnology.io>
2022-11-02 17:19:28 +0700

    remove packages installation from build script

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -3,9 +3,3 @@ mkdir -p /opt/Coreograph/python
 sudo chown -R $USER:$USER /opt/Coreograph/python
 ./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --with-openssl=../openssl/openssl-install --enable-shared --enable-optimizations
 make clean; make altinstall
-
-# upgrade pip
-/opt/Coreograph/python/bin/python3.12 -m pip install --upgrade pip
-
-# thrid-party libs
-/opt/Coreograph/python/bin/python3.12 -m pip install opencv-python-headless

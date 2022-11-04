bfbe4cb5781816293d2e4114df58a33c8d845f90
prachkh <prach@airtechnology.io>
2022-11-04 20:00:30 +0700

    set openssl rpath to python runtime installation directory

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,5 +1,18 @@
 rm -rf /opt/Coreograph/python
 mkdir -p /opt/Coreograph/python
-sudo chown -R $USER:$USER /opt/Coreograph/python
-./configure --with-ensurepip=install --prefix /opt/Coreograph/python --exec-prefix /opt/Coreograph/python --with-openssl=../openssl/openssl-install --enable-shared --enable-optimizations
+
+./configure --with-ensurepip=install \
+    --prefix /opt/Coreograph/python \
+    --exec-prefix /opt/Coreograph/python \
+    --with-openssl=../openssl/openssl-install \
+    --with-ssl-default-suites=openssl \
+    CFLAGS="-I../openssl/openssl-install/include" \
+    LDFLAGS="-Wl,-rpath,/opt/Coreograph/python/openssl/" \
+    --enable-shared \
+    --enable-optimizations
+
 make clean; make altinstall
+
+mkdir -p /opt/Coreograph/python/openssl
+cp ../openssl/openssl-install/lib64/libcrypto.so.3 /opt/Coreograph/python/openssl
+ln -s /opt/Coreograph/python/openssl/libcrypto.so.3 /opt/Coreograph/python/openssl/libcrypto.so 
